cc0bd3b108df2ef7853489edbd293382328efc22
rismanitalst <rismanitalst@gmail.com>
2025-10-04 19:01:41 +0700

    menghapus file tidak perlu

--- bin/practice_3.dart
+++ bin/practice_3.dart
@@ -1,5 +1,3 @@
-import 'package:practice_3/practice_3.dart' as practice_3;
-
-void main(List<String> arguments) {
-  print('Hello world: ${practice_3.calculate()}!');
+void main() {
+  print('Hello World !!!');
 }
